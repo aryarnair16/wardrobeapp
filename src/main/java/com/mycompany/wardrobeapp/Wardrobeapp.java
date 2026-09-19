2ee9071be930c55fa5ac40a25ec27a00e1c9a138
Arya R Nair <aryarajesh1603@gmail.com>
2026-09-19 21:19:16 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/wardrobeapp/Wardrobeapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wardrobeapp;
+
+/**
+ *
+ * @author aryar
+ */
+public class Wardrobeapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
